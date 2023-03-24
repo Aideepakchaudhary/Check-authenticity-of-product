5a6d4767d1b2108893b7f19a35a6e01be36607dd
aideepakchaudhary <aideepakchaudhary@gmail.com>
2023-03-24 23:18:03 +0530

    doc-added

--- pallets/template/src/lib.rs
+++ pallets/template/src/lib.rs
@@ -1,14 +1,6 @@
 #![cfg_attr(not(feature = "std"), no_std)]
 
 
-// Only manufacturing unit will allow to add all the hash of the product..
-// Check if hash is found in the storage that means the product is authentic.
-// Should have mechanism to add manufacturing unit
-// Need to think..if a product is used then do we need to delete the product from the chain?
-// Anybody can see the barcode on the product and try to copy that barcode and put on the fake product?
-// how to generate a unique hash?
-// What are the details required to add the manufacturer?
-
 //! We Need to store the products in a different storage(partially_selled_products) which is scanned first..so that no one can copy the product QR.
 //! If a user wants to return the Product and we have 15 days for it.until then this product is stored in partially_selled_products.
 //! After the return time is over the product goes to sell_product.
